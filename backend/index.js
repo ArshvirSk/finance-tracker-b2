d1822d6ccd62b0000b5e66dac6921cf0924bdc33
Arshvir Singh Kalsi <arshvirsk26@gmail.com>
2025-07-07 17:30:54 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(
   cors({
     origin: {
-      "http://localhost:3000": true, // Allow local development
+      "http://localhost:5173": true, // Allow local development
     },
   })
 ); // Allow cross-origin requests
